f7235bf97446818edb0077c9b71e00cf94ce69df
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-02-18 10:35:28 +0100

    Lettre G

--- js/services/AlphabetService.js
+++ js/services/AlphabetService.js
@@ -11,12 +11,8 @@ app.service('AlphabetService', function(){
             "d" : function(x,y){creerLigneV(x,y,"black",5);creerLigneV(x+2,y+1,"black",3);creerLigneH(x+1,y,"black",1);creerLigneH(x+1,y+4,"black",1);},
             "e" : function(x,y){creerLigneV(x,y,"black",5);creerLigneH(x,y,"black",3);creerLigneH(x,y+2,"black",2);creerLigneH(x,y+4,"black",3);},
             "f" : function(x,y){creerLigneV(x,y,"black",5);creerLigneH(x,y,"black",3);creerLigneH(x,y+2,"black",2);},
-            "g" : function(x,y){creerLigneV(x,y+1,"black",3);creerLigneV(x+3,y+2,"black",2);creerLigneH(x+1,y,"black",2);creerLigneH(x+1,y+4,"black",2);creerLigneH(x+2,y+2,"black",1);
-        
-            },
-            
-            "i" : function(x,y){creerLigneV(x, y, "black", 5);},
-          
+            "g" : function(x,y){creerLigneV(x,y+1,"black",3);creerLigneV(x+3,y+2,"black",2);creerLigneH(x+1,y,"black",2);creerLigneH(x+1,y+4,"black",2);creerLigneH(x+2,y+2,"black",1);},            
+            "i" : function(x,y){creerLigneV(x, y, "black", 5);},          
             "u": function(x, y){creerLigneV(x, y, "black", 4);creerLigneV(x+3, y, "black", 4);creerLigneH(x+1, y+4, "black", 2)
         ;
     }};
