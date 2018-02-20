ff2977d530448158f307862f4512e737e36284fa
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-02-21 00:04:32 +0100

    Ajout addLine addColum

--- js/controllers/PlanController.js
+++ js/controllers/PlanController.js
@@ -2,13 +2,15 @@ app.controller('PlanController', function($scope){
     const canvasPlan = document.getElementById('plan');
     const ctxPlan = canvasPlan.getContext('2d');
     const mousePosition = { x: window.innerWidth/2, y: window.innerHeight/2 };
-    const premierPlan = 1, secondPlan = 2, fondPlan = 3;
+    const premierPlan = 1, secondPlan = 2, fondPlan = 3;    
     let width = window.innerWidth, height = window.innerHeight;
     let caddrillage = [];
     let size = 5;
-    let sizeMoins = size - 1;
+    let sizeMoins = size;
     canvasPlan.height = height;
     canvasPlan.width = width;    
+    const nbCaseW = Math.floor(width / size);
+    const nbCaseH = Math.floor(height / size);
 
     orderPassageAsc = function(x){
         let min = x[0];
@@ -32,7 +34,7 @@ app.controller('PlanController', function($scope){
 
     for(var y = 0; y < height / size; y++){
         for(var x = 0; x < width / size; x++){
-            caddrillage.push([x * size, y * size, size-1, size-1, [['white', 4]]]);
+            caddrillage.push([x * size, y * size, sizeMoins, sizeMoins, [['white', 4]]]);
         }
     }
 
@@ -42,16 +44,26 @@ app.controller('PlanController', function($scope){
         ctxPlan.fillRect(caddrillage[i][0], caddrillage[i][1],  caddrillage[i][2], caddrillage[i][3]);
     }
 
+    window.addEventListener('click', function(e) {
+        var tailleMaxW = window.innerWidth, tailleMaxH = window.innerHeight;
+        mousePosition.x = e.pageX;
+        mousePosition.y = e.pageY;
+        var caseCourante = (Math.floor(mousePosition.x * nbCaseW / tailleMaxW) +
+        Math.floor(mousePosition.y * nbCaseH / tailleMaxH) * nbCaseW);
+        console.log("Case n° "+ caseCourante);
+    });
+
     addPixel = function(num, color, order){
         var found = caddrillage[0][4].find(function(element){
             if (element[1] == order){
                 element[0] = color;
-                return true;
+                return;
             }
         });
         if(found == null){
             caddrillage[num][4].push([color, order])
         }
+        console.log(orderPassageAsc(caddrillage[num][4]));
         ctxPlan.fillStyle = orderPassageAsc(caddrillage[num][4]);
         ctxPlan.fillRect(caddrillage[num][0], caddrillage[num][1],  caddrillage[num][2], caddrillage[num][3]);
         
@@ -69,15 +81,20 @@ app.controller('PlanController', function($scope){
         
     };
 
-    window.addEventListener('click', function(e) {
-        var tailleMaxW = window.innerWidth, tailleMaxH = window.innerHeight;
-        mousePosition.x = e.pageX;
-        mousePosition.y = e.pageY;        
-        var nbCaseW = Math.floor(tailleMaxW / size);
-        var nbCaseH = Math.floor(tailleMaxH / size);
-        var caseCourante = (Math.floor(mousePosition.x * nbCaseW / tailleMaxW) +
-        Math.floor(mousePosition.y * nbCaseH / tailleMaxH) * nbCaseW);
-        console.log("Case n° "+ caseCourante);
-    });
+    addLine = function(num, color, order, pas){
+        for(var i = 0; i < pas; i++){
+            addPixel(num + i, color, order);
+        }
+    };
+
+    addColumn = function(num, color, order, pas){
+        for(var i = 0; i < pas; i++){
+            console.log(num + i * nbCaseW, color);
+            addPixel(num + i * nbCaseW, color, order);
+        }
+    };
+
+    addColumn(0, "green", 2, 5);
+    
 });
 
