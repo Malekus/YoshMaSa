4e68a96f3655d756f3ebb8338f4c60a6d5ef8952
ChefDeProjet <QueDesPiratesIci@piratus.net>
2018-03-23 11:21:27 +0100

    update option

--- js/controller/OptionController.js
+++ js/controller/OptionController.js
@@ -1,5 +1,10 @@
 app.controller("OptionController", function($scope, $rootScope){
     $scope.heightScreen = $rootScope.height;
     $scope.widthScreen = $rootScope.width;
+
+    $scope.couleur = false;
+    $scope.on_off = function(){
+      $scope.couleur = !$scope.couleur;
+    }
     
 });
